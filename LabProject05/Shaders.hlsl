71c92deeaa155d3dd0db82814b5f3f549dd93f9c
roal518 <roalqwerty@gmail.com>
2024-05-25 13:26:44 +0900

    시작 1
    분명 같은 코드를 쓰고 있는데 어디서 잘못됐는지 이건 되고 저건 안되고 개판이다. 너무 어렵다

---
+++ LabProject05/Shaders.hlsl
@@ -0,0 +1,23 @@
+struct VS_INPUT
+{
+    float3 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct VS_OUTPUT
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+VS_OUTPUT VSMain(VS_INPUT input)
+{
+    VS_OUTPUT output; 
+    output.position = float4(input.position, 1.0f);
+    output.color = input.color;
+    return(output);
+}
+float4 PSMain(VS_OUTPUT input) : SV_TARGET
+{
+    return(input.color);
+}
